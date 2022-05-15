c354631b78ccd219059763d52407a00f4d12cae9
Manishsingh183 <84369412+Manishsingh183@users.noreply.github.com>
2022-05-15 22:25:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const cors = require("cors");
 const session = require('express-session');
 const passport = require('passport');
 const dotenv = require('dotenv').config();
-// const findOrCreate = require('mongoose-findorcreate')
+const multer = require('multer');
+var fs = require('fs');
+var path = require('path');
+// const findOrCreate = require('mongoose-findorcreate');
+// const { authenticate } = require('passport');
 
 
 app.use(cors());
@@ -16,7 +20,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 // mongoose.connect("mongodb+srv://admin-prat33k:admin123@cluster0.4wyjr.mongodb.net/campusQ-db");
 
 // app.use(session({
-//     Secret: process.env.SESSION_SECRET,
+//     secret: process.env.SESSION_SECRET,
 //     resave: false,
 //     saveUninitialized: false
 // }));
@@ -69,9 +73,7 @@ const projSchema = new mongoose.Schema({
 const Post = new mongoose.model("Post", postSchema);
 const Project = new mongoose.model("Project",projSchema);
 
-// passport.use(Post.createStrategy());
-// passport.serializeUser(Post.serializeUser());
-// passport.deserializeUser(Post.deserializeUser());
+
 
 // passport.serializeUser(function(user, done) {
 //     done(null, user.id); 
@@ -98,19 +100,47 @@ const Project = new mongoose.model("Project",projSchema);
 //   }
 // ));
 
+// var storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads')
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, Date.now() + '-'+  file.originalname )
+//     }
+// });
+  
+// var upload = multer({ storage}).single('file');
 
-app.post("/askque",async (req,res)=>{
+// app.post('/askque',(req,res)=>{
+//     console.log(req.body);
+//     upload(req,res,(err)=>{
+//         if(err){
+//             return res.status(500).json(err)
+//         }
+//         return res.status(200).send(req.file)
+//     })
+// });
+
+
+
+
+app.post("/askque",async(req,res)=>{
     const data = req.body;
-    console.log(data.question);
+    console.log(req.body);
     const newPost = new Post({
         key: data._id,
         question: data.question,
-        answers: ["In anim laborum minim dolore reprehenderi."],
+        answers: ["In anim laborum minim dolore reprehenderi.","aksljnbkdjvbv","sadkjbvnfkjlbndsf"],
         likes: 5,
         dislikes: 1,
         comments: [],
         date: date,
         time: time,
+        // img: {
+        //     data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+        //     contentType: 'image/png'
+        // }
+        
     });
     Post.create(newPost,(err)=>{
         if(err){
@@ -142,13 +172,28 @@ app.post("/add_proj",async (req,res)=>{
     })
 });
 
-let getPost = '';
+app.post("/newans",async (req,res)=>{
+    const data = req.body;
+    console.log(data);
+    Post.findOneAndUpdate(
+        { _id: data.id }, 
+        { $push: { answers: data.ans } },
+        function (err, success) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("done");
+            }
+        }
+    );
+})
 
-app.post("/collab",(req,res)=>{
-       getPost = req.body;
-       console.log("Collab back-end")
-       console.log(getPost);
-});
+
+
+
+app.post('/posts',(req,res)=>{
+    // console.log(req.body);
+})
 
 
 
@@ -166,6 +211,21 @@ app.get("/",function(req,res){
     })
 });
 
+// app.get("/auth/google",(req,res)=>{
+//     passport.authenticate("google",{scope:["profile"]});
+//     console.log("Auth area");
+// });
+
+// app.get('/auth/google/askque', 
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     // Successful authentication, redirect home.
+//     console.log("Auth comleted in auth/google/askque");
+//     res.redirect('/askque');
+
+//   });
+
+
 app.get("/collab", (req,res) => {
     Project.find({},(err,foundItem) => {
         if(err){
@@ -177,20 +237,38 @@ app.get("/collab", (req,res) => {
     })
 })
 
-app.get("/post",(req,res)=>{
-    console.log("get request in /post with id => ")
-    Post.find({'_id':{$in : mongoose.Types.ObjectId(getPost.id_details)}},(err,foundItem)=>{
+app.get("/posts/:Id",(req,res)=>{
+    const postId = req.params.Id;
+    // console.log("get request in /post/ with id => ",postId);
+    Post.findById(postId,(err,foundItem)=>{
         if(err){
             console.log("cant get data");
             console.log(err);
         }
         else{
             console.log("got data");
-            console.log(foundItem);
             res.send(foundItem);  
         }
-    })
-})
+    }).clone().catch(function(err){ console.log(err)})
+});
+
+// 
+// const url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token="+token;
+// app.get(url,(req,res)=>{
+//     console.log(req);
+// })
+
+
+// let token = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg2MTY0OWU0NTAzMTUzODNmNmI5ZDUxMGI3Y2Q0ZTkyMjZjM2NkODgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiMjcxNDMxODQ0MjE1LXNpMjd2NXNoMnE0Z3Q3dGE2anAzaWVncGRycmVxbTBlLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiMjcxNDMxODQ0MjE1LXNpMjd2NXNoMnE0Z3Q3dGE2anAzaWVncGRycmVxbTBlLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTEwNjQzMDI5MDQ4OTMzNTEwNzMyIiwiZW1haWwiOiJtYW5pc2hzaW5naDkwNTE2MTJAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJMRmJ3X2JKdmY2WGNaR3hHaXp2QXZBIiwibmFtZSI6IkRlY2F0aCBsb2dvbiIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQVRYQUp3WjY5QnFjZVVlTWMxU25LS0p3MXhiZlg4SUdvMTJtVlY1YnBtUD1zOTYtYyIsImdpdmVuX25hbWUiOiJEZWNhdGgiLCJmYW1pbHlfbmFtZSI6ImxvZ29uIiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2NTE0MzE0ODEsImV4cCI6MTY1MTQzNTA4MSwianRpIjoiZTYzNGQxNGM1ZTdiMzM5MTVjZDc1MzZhNTc0Yzk3ZmU5MDdmMWFiYyJ9.CjPlon_PCANWBsywveYdHSeDTYUyS1IsIuqVCNcm6jNC_QWgQaUvOAwaYGVH-OhUdX5rRI_oJfHQYJjECiREwWeGrjuj-BfcB6XsDJfAq-sRIy2eQEvh4hu7UkFKbZyDdAX895SF1Hv81EXCdu71A5CT0AZu9ep1GFaYkRmDOqCBTL9Ytgm9ioOmxBd1u7gbFJU0LfMcpbY0sQiyRfJi-JKLkwvF_DsOAGUoiy1L0Fd3IDbYlQZ9jAYXQqClcO4eVwaqgSDCLVcpe4TRWnTMyRlrdSpxtGr84E-qtJi8pUuFy_gLVy5k_zH4X6nUekHL-KvS90yFwSkMo2QpLpqfrQ"
+
+// app.get("/askque",(req,res)=>{
+//     if(req.isAuthenticated()){
+//         console.log("Authentication completed");
+//         res.render('askque');
+//     }else{
+//         res.redirect('/login');
+//     }
+// });
 
 
 
